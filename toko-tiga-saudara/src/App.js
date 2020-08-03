60e69f8b273f8989dff1054108c27576ebf8d951
dennistutuko <tutukodennis@gmail.com>
2020-08-04 02:54:41 +0700

    added the sign-in page and animations, user-auth pending

--- toko-tiga-saudara/src/App.js
+++ toko-tiga-saudara/src/App.js
@@ -6,12 +6,9 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
@@ -20,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
     </div>
   );
