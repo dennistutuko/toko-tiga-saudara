bfc3377ab81edb528a7a5d75423d4bc826905661
dennistutuko <tutukodennis@gmail.com>
2020-08-03 17:20:09 +0700

    added preview collection page on /shop

--- toko-tiga-saudara/src/App.js
+++ toko-tiga-saudara/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -16,8 +17,9 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
-      </Switch>
+        <Route path='/shop' component={ShopPage} />
+        </Switch>
+        <Route path='/shop/hats' component={HatsPage} />
     </div>
   );
 };
