45e2df92487ad59fe806ef761c622bf8d8ea0686
dennistutuko <tutukodennis@gmail.com>
2020-08-06 18:19:20 +0700

    added changes from lesson 9 repo

--- toko-tiga-saudara/src/App.js
+++ toko-tiga-saudara/src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -15,17 +15,30 @@ class App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
   }
-  
-unsubscribeFromAuth = null
+
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    } )
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount() {
@@ -35,15 +48,15 @@ unsubscribeFromAuth = null
   render() {
     return (
       <div>
-      <Header currentUser={this.state.currentUser} />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route path='/signin' component={SignInAndSignUpPage} />
-          </Switch>
+        </Switch>
       </div>
     );
   }
-};
+}
 
 export default App;
--- toko-tiga-saudara/src/firebase/firebase.utils.js
+++ toko-tiga-saudara/src/firebase/firebase.utils.js
@@ -3,16 +3,41 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-        apiKey: "AIzaSyAcoV852L74zQCjFTtnkA9UsDGUbnc_o-I",
-        authDomain: "crwn-db-20e7d.firebaseapp.com",
-        databaseURL: "https://crwn-db-20e7d.firebaseio.com",
-        projectId: "crwn-db-20e7d",
-        storageBucket: "crwn-db-20e7d.appspot.com",
-        messagingSenderId: "4062774435",
-        appId: "1:4062774435:web:2b32f4729a84a7be912b16"
+  apiKey: "AIzaSyAcoV852L74zQCjFTtnkA9UsDGUbnc_o-I",
+  authDomain: "crwn-db-20e7d.firebaseapp.com",
+  databaseURL: "https://crwn-db-20e7d.firebaseio.com",
+  projectId: "crwn-db-20e7d",
+  storageBucket: "crwn-db-20e7d.appspot.com",
+  messagingSenderId: "4062774435",
+  appId: "1:4062774435:web:2b32f4729a84a7be912b16"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
